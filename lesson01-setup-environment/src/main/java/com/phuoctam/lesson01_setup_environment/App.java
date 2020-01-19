031151594741c7d2aa2668cf70b843594afe61f8
phuoc-tam <phuoctam2002@gmail.com>
2020-01-19 17:58:00 +0700

    lesson01-source

---
+++ lesson01-setup-environment/src/main/java/com/phuoctam/lesson01_setup_environment/App.java
@@ -0,0 +1,11 @@
+package com.phuoctam.lesson01_setup_environment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
